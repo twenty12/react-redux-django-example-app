46dc4c293d598df8e36cfbebd9decbdb3cef2c3d
daniel.gladstone <gladstone.d@gmail.com>
2020-11-21 19:34:55 -0500

    styles and dropdwon added

--- frontend/app/src/features/accounts/manageAccounts.js
+++ frontend/app/src/features/accounts/manageAccounts.js
@@ -1,8 +1,76 @@
+import { func } from "prop-types";
 import React from "react";
-// import styles from "./user.module.css";
 
+const users = [
+    {
+        userId: 1,
+        firstName: "Jon",
+        lastName: "Doe",
+      },
+      {
+        userId: 2,
+        firstName: "Jane",
+        lastName: "Doe",
+      },
+      {
+        userId: 3,
+        firstName: "Jon",
+        lastName: "Doe",
+      },
+      {
+        userId: 4,
+        firstName: "Jane",
+        lastName: "Doe",
+      }
+]
+const accountData = [
+  {
+    accountId: 1,
+    users: [users[0], users[1]],
+  },
+  {
+    accountId: 2,
+    users: [users[2], users[3]],
+  },
+];
+
+
+
+export function UserDropDown() {
+    function toggleDropdown(e) {
+        console.log(e)
+    }
+    return (
+        <div className="dropdown">
+        <button onClick={toggleDropdown} className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown button
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item d-none bg-light" href="#">Action</a>
+            <a className="dropdown-item d-none bg-light" href="#">Another action</a>
+            <a className="dropdown-item d-none bg-light" href="#">Something else here</a>
+        </div>
+        </div>
+    )
+}
 export function ManageAccounts() {
   return (
-    <h1>Get to managing</h1>
+    <ul className="list-group">
+      {accountData.map((account) => {
+        return (
+            <li className="list-group-item" key={account.accountId}>
+                Account ID {account.accountId}
+                <ul className="list-group">
+                    {account.users.map((user) => {
+                        return (
+                        <li className="list-group-item" key={user.userId}>{user.firstName} {user.lastName}</li>
+                        )
+                    })}
+                    <li className="list-group-item" key="addUser"><UserDropDown/></li>
+                </ul>
+            </li>
+        )
+      })}
+    </ul>
   );
 }
